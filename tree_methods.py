a0584445cfdf466df05daaffeb63027ffce9b46f
Matthew Kennedy <MatthewKennedy2021@u.northwestern.edu>
2019-04-11 13:00:15 -0500

    Added Xin's changes to the tree methods code.

--- tree_methods.py
+++ tree_methods.py
@@ -10,7 +10,6 @@ import itertools
 import random
 
 class Node(object):
-
     def __init__(self, vector, name=None, structure=None, left_child=None, right_child=None, parent=None):
         self.vector = vector
         self.name = name
@@ -19,6 +18,15 @@ class Node(object):
         self.rightchild = right_child
         self.parent = None
 
+    def __str__(self):
+        s = '''
+            Name: %s
+            structure: %s
+            leftchild: %s
+            rightchild: %s
+            parent: %s
+        '''%(self.name, str(self.structure), self.leftchild.name, self.rightchild.name, str(self.parent.name))
+        return s
 
 def depth(node):
     '''
@@ -53,84 +61,19 @@ def GRUtree_pytorch(x, h, weight_ih_l0, weight_hh_l0, bias_ih_l0, bias_hh_l0):
     z_1Node = Node(z,  name='z1', left_child=Node(x, 'x'), right_child=Node(h, 'h'))
     rNode = Node(r, name='r', left_child=Node(x, 'x'), right_child=Node(h, 'h'))
     z_2Node = Node(z,  name='z2', left_child=Node(x, 'x'), right_child=Node(h, 'h'))
-    rhNode = Node(rh, name='r*h', left_child=rNode, right_child=Node(h, 'h'))
-    h_tildeNode = Node(h_tilde, name='h_tilde', left_child=rhNode, right_child=Node(x, 'x'))
-    oneMinuszNode = Node(oneMinusz, name='1-z', left_child=z_1Node, right_child=Node(o, '0'))
+    rhNode = Node(rh, name='r*h', left_child=Node(h, 'h'), right_child=rNode)
+    h_tildeNode = Node(h_tilde, name='h_tilde', left_child=Node(x, 'x'), right_child=rhNode)
+    oneMinuszNode = Node(oneMinusz, name='1-z', left_child=Node(o, '0'), right_child=z_1Node)
     zh_tildeNode = Node(zh_tilde, name='(1-z)*h_tilde', left_child=h_tildeNode, right_child=oneMinuszNode)
-    zhNode = Node(zh, name='z*h', left_child=z_2Node, right_child=Node(h, 'h'))
+    zhNode = Node(zh, name='z*h', left_child=Node(h, 'h'), right_child=z_2Node)
     h_nextNode = Node(h_next, name='h_next', left_child=zh_tildeNode, right_child=zhNode)
 
-    rNode.parent = rhNode
-    rhNode.parent = h_tildeNode
-    h_tildeNode.parent = zh_tildeNode
-    z_1Node.parent = oneMinuszNode
-    z_2Node.parent = zhNode
-    zhNode.parent = h_nextNode
-    oneMinuszNode.parent = zh_tildeNode
-    zh_tildeNode.parent = h_nextNode
-    rNode.leftchild.parent = rNode
-    rNode.rightchild.parent = rNode
-    rhNode.rightchild.parent = rhNode
-    h_tildeNode.rightchild.parent = h_tildeNode
-    z_1Node.leftchild.parent = z_1Node
-    z_1Node.rightchild.parent = z_1Node
-    z_2Node.leftchild.parent = z_2Node
-    z_2Node.rightchild.parent = z_2Node
-    zhNode.rightchild.parent = zhNode
-    oneMinuszNode.rightchild.parent = oneMinuszNode
-
-    
-#    node_list = [rNode, z_1Node, z_2Node, rhNode, h_tildeNode, zhNode, oneMinuszNode, zh_tildeNode, h_nextNode]
     node_list = [z_1Node, rNode, z_2Node, rhNode, h_tildeNode, oneMinuszNode, zh_tildeNode, zhNode, h_nextNode]
-
+    for node in node_list:
+        node.leftchild.parent = node
+        node.rightchild.parent = node
+        
     return h_nextNode, node_list
-
-
-#
-#def GRUtree(x, h, Lr, Rr, Lz, Rz, Lh, Rh, br, bz, bh):
-#    o = torch.zeros(x.shape)
-#    r = torch.sigmoid(torch.mm(x, Lr) + torch.mm(h, Rr) + br)
-#    z = torch.sigmoid(torch.mm(x, Lz) + torch.mm(h, Rz) + bz)
-#    rh = r*h
-#    z1 = 1-z
-#    h_tilde = torch.tanh(torch.mm(x, Lh) + torch.mm(rh, Rh) + bh)
-#    zh = z*h
-#    zh_tilde = z1*h_tilde
-#    h_next = zh+zh_tilde
-#
-#    rNode = Node(r, name='r', left_child=Node(x, 'x'), right_child=Node(h, 'h'))
-#    rhNode = Node(rh, name='r*h', left_child=rNode, right_child=Node(h, 'h'))
-#    h_tildeNode = Node(h_tilde, name='h_tilde', left_child=rhNode, right_child=Node(x, 'x'))
-#    z1Node = Node(z,  name='z1', left_child=Node(x, 'x'), right_child=Node(h, 'h'))
-#    z2Node = Node(z,  name='z2', left_child=Node(x, 'x'), right_child=Node(h, 'h'))
-#    zhNode = Node(zh, name='z*h', left_child=z1Node, right_child=Node(h, 'h'))
-#    oneMinuszNode = Node(z1, name='1-z', left_child=z2Node, right_child=Node(o, '0'))
-#    zh_tildeNode = Node(z1*h_tilde, name='(1-z)*h_tilde', left_child=h_tildeNode, right_child=oneMinuszNode)
-#    h_nextNode = Node(h_next, name='h_next', left_child=zh_tildeNode, right_child=zhNode)
-#
-#    rNode.parent = rhNode
-#    rhNode.parent = h_tildeNode
-#    h_tildeNode.parent = zh_tildeNode
-#    z1Node.parent = zhNode
-#    z2Node.parent = oneMinuszNode
-#    zhNode.parent = h_nextNode
-#    oneMinuszNode.parent = zh_tildeNode
-#    zh_tildeNode.parent = h_nextNode
-#    rNode.leftchild.parent = rNode
-#    rNode.rightchild.parent = rNode
-#    rhNode.rightchild.parent = rhNode
-#    h_tildeNode.rightchild.parent = h_tildeNode
-#    z1Node.leftchild.parent = z1Node
-#    z1Node.rightchild.parent = z1Node
-#    z2Node.leftchild.parent = z2Node
-#    z2Node.rightchild.parent = z2Node
-#    zhNode.rightchild.parent = zhNode
-#    oneMinuszNode.rightchild.parent = oneMinuszNode
-#
-#    node_list = [rNode, z1Node, z2Node, rhNode, h_tildeNode, zhNode, oneMinuszNode, zh_tildeNode, h_nextNode]
-#    return h_nextNode, node_list
-
-
 
 
 def label(tree, l=1):
