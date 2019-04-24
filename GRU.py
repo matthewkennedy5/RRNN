e7c2b5340eaa6ed846f39b8e5dfa088eb7cb1c54
Xin Qian <44562654+Burururu@users.noreply.github.com>
2019-04-24 17:53:54 -0500

    Optimized forward pas

--- GRU.py
+++ GRU.py
@@ -37,6 +37,10 @@ def retrieve_unary_operation(s):
         raise ValueError('No such unary function %s!'%s)
     return unary
 
+def margins_func(scores):
+    sorted_scores = torch.sort(scores)[0]
+    return sorted_scores[:, :, -1] - sorted_scores[:, :, -2]
+
 
 class RRNNforGRUCell(nn.Module):
     def __init__(self, hidden_size, scoring_hsize=None):
@@ -82,27 +86,29 @@ class RRNNforGRUCell(nn.Module):
                            )
         else:
             self.scoring = nn.Linear(hidden_size, 1, bias=False)
-
+    
+    
     def forward_stage_search(self, x, h_prev):
         batch_size, _, hidden_size = x.shape
-        o = torch.zeros_like(x, device=x.shape)
+        o = torch.zeros_like(x, device=x.device)
         l = self.l
         softmax_func = torch.nn.Softmax(dim=2)
         
         G = []
+        G_node = []
         G_structure = []
-        G_structure_second = []
+        G_margin = []
         components_list = []
-        
+             
         for r in range(self.N):
             candidate = [x, h_prev, o] + [comp.vector for comp in components_list]
-            L_times_left_child = [[torch.matmul(candidate[i], self.L_list[k]) for i in range(len(candidate))] for k in range(l)]
-            R_times_left_child = [[torch.matmul(candidate[j], self.R_list[k]) for j in range(len(candidate))] for k in range(l)]
+            L_times_left_child = [[torch.matmul(candidate[i], self.L_list[k]) for k in range(l)] for i in range(len(candidate))]
+            R_times_left_child = [[torch.matmul(candidate[j], self.R_list[k]) for k in range(l)] for j in range(len(candidate))]
             V = []
             V_structure = []
                         
             for i in range(len(candidate)-1):
-                for j in range(i, len(candidate)):
+                for j in range(i+1, len(candidate)):
                     flag = 0
                     if (self.N - r) == len(components_list) - 1: # You could only merge two components
                         if i >= 3:
@@ -149,167 +155,124 @@ class RRNNforGRUCell(nn.Module):
             
             V = torch.cat(V, dim=1)
             scores = self.scoring(V).reshape(batch_size, 1, -1)
-            weighted_vectors = (scores*V).sum(dim=1).reshape(batch_size, 1, hidden_size)
+            weighted_vector = torch.matmul(softmax_func(scores), V)
+            margin = margins_func(scores)
+            structure = V_structure[torch.argmax(scores.sum(dim=0)).item()]
             
-            
-            
-            # scoring
-                                    
-                    
+            G.append(weighted_vector)
+            G_margin.append(margin)
                         
+            i, j = structure[:2]
+            name_list = ['x', 'h_prev', 'o'] + ['G[%d]'%i for i in range(r)]
+            if i>2 and j>2: # merged two components
+                left_node = components_list[i-3]
+                right_node = components_list[j-3]
+                new_component = Node(weighted_vector, name='G[%d]'%r, 
+                                     structure=[left_node.name, right_node.name]+structure[2:],
+                                     left_child=left_node, right_child=right_node)
+                components_list[i-3] = new_component
+                del components_list[j-3]
+            elif i<=2 and j>2:
+                left_node = Node(candidate[i], name=name_list[i])
+                right_node = components_list[j-3]
+                new_component = Node(weighted_vector, name='G[%d]'%r, 
+                                     structure=[left_node.name, right_node.name]+structure[2:],
+                                     left_child=left_node, right_child=right_node)
+                components_list[j-3] = new_component
+            else:   # elif i<=2 and j<=2:
+                left_node = Node(candidate[i], name=name_list[i])
+                right_node = Node(candidate[j], name=name_list[j])
+                new_component = Node(weighted_vector, name='G[%d]'%r, 
+                                     structure=[left_node.name, right_node.name]+structure[2:],
+                                     left_child=left_node, right_child=right_node)                
+                components_list.append(new_component)
             
+            G_structure.append(new_component.structure)
+            G_node.append(new_component)
         
+        G_margin = torch.cat(G_margin, dim=1)
+        return G[-1], G_structure, G_node, G_margin
         
+    def foward_stage_fixing(self, x, h_prev, G_structure):
+        o = torch.zeros_like(x, device=x.device)
+        G = []
+    
+        for structure in G_structure:
+            left_name, right_name, k, binary_func, unary_func = structure
+            unary_func = retrieve_unary_operation(unary_func)
+            binary_func = retrieve_binary_operation(binary_func)
+            L, R, b = self.L_list[k], self.R_list[k], self.b_list[k]
+            v = unary_func(binary_func(torch.matmul(eval(left_name), L), torch.matmul(eval(right_name), R))+b)
+            G.append(v)
         
-        
-        
-        
-        return 
-
-    def forward(self, x, h_prev):
-        """ Batched version of forward pass
-
-        Input:
-            x - Tensor of shape batch_size * 1 * hidden_size
-            h_prev - Tensor of shape batch_size * 1 * hidden_size
-        """
-        # initialize L3 R3 to be identity
-        batch_size = x.shape[0]
-        l = self.l
-        margins = []
-        G_structure = np.zeros((batch_size, self.N))
-        softmax_func = torch.nn.Softmax(dim=2)
-
-        z_1 = torch.sigmoid(torch.matmul(x, self.L_list[0]) + torch.matmul(h_prev, self.R_list[0]) + self.b_list[0])
-
-        r = torch.sigmoid(torch.matmul(x, self.L_list[1]) + torch.matmul(h_prev, self.R_list[1]) + self.b_list[1])
-
-        z_2 = torch.sigmoid(torch.matmul(x, self.L_list[0]) + torch.matmul(h_prev, self.R_list[0]) + self.b_list[0])
-
-        # h*r
-        lst = torch.zeros([batch_size, l, self.hidden_size], device=x.device)
-        for k in range(l):
-            lst[:, k, :] = (torch.matmul(h_prev, self.L_list[k]) * torch.matmul(r, self.R_list[k]) + self.b_list[k]).squeeze(dim=1)
-        scores = self.scoring(lst).reshape(batch_size, 1, l)
-        rh = torch.matmul(softmax_func(scores), lst)
-        G_structure[:, 3] = torch.argmax(scores, dim=2).squeeze().tolist()
-        margins.append(self.margin(scores))
-
-        # h_tilde
-        lst = torch.zeros([batch_size, l, self.hidden_size], device=x.device)
-        for k in range(l):
-            lst[:, k, :] = (torch.tanh(torch.matmul(x, self.L_list[k]) + torch.matmul(rh, self.R_list[k]) + self.b_list[k])).squeeze(dim=1)
-        scores = self.scoring(lst).reshape(batch_size, 1, l)
-        h_tilde = torch.matmul(softmax_func(scores), lst)
-        G_structure[:, 4] = torch.argmax(scores, dim=2).squeeze().tolist()
-        margins.append(self.margin(scores))
-
-        #oneMinusz
-        lst = torch.zeros([batch_size, l, self.hidden_size], device=x.device)
-        for k in range(l):
-            lst[:, k, :] = (1 - (torch.matmul(z_1, self.R_list[k]) + self.b_list[k])).squeeze(dim=1)
-        scores = self.scoring(lst).reshape(batch_size, 1, l)
-        oneMinusZ1 = torch.matmul(softmax_func(scores), lst)
-        G_structure[:, 5] = torch.argmax(scores, dim=2).squeeze().tolist()
-        margins.append(self.margin(scores))
-
-        # (1-z)*h_tilde
-        lst = torch.zeros([batch_size, l, self.hidden_size], device=x.device)
-        for k in range(l):
-            lst[:, k, :] = (torch.matmul(h_tilde, self.L_list[k])*torch.matmul(oneMinusZ1, self.R_list[k]) + self.b_list[k]).squeeze(dim=1)
-        scores = self.scoring(lst).reshape(batch_size, 1, l)
-        zh_tilde = torch.matmul(softmax_func(scores), lst)
-        G_structure[:, 6] = torch.argmax(scores, dim=2).squeeze().tolist()
-        margins.append(self.margin(scores))
-
-        # z2*h
-        lst = torch.zeros([batch_size, l, self.hidden_size], device=x.device)
-        for k in range(l):
-            lst[:, k, :] = (torch.matmul(h_prev, self.L_list[k])*torch.matmul(z_2, self.R_list[k]) + self.b_list[k]).squeeze(dim=1)
-        scores = self.scoring(lst).reshape(batch_size, 1, l)
-        z2h = torch.matmul(softmax_func(scores), lst)
-        G_structure[:, 7] = torch.argmax(scores, dim=2).squeeze().tolist()
-        margins.append(self.margin(scores))
-
-        # h_t
-        lst = torch.zeros([batch_size, l, self.hidden_size], device=x.device)
-        for k in range(l):
-            lst[:, k, :] = (torch.matmul(zh_tilde, self.L_list[k]) + torch.matmul(z2h, self.R_list[k]) + self.b_list[k]).squeeze(dim=1)
-        scores = self.scoring(lst).reshape(batch_size, 1, l)
-        h_next = torch.matmul(softmax_func(scores), lst)
-        G_structure[:, 8] = torch.argmax(scores, dim=2).squeeze().tolist()
-        margins.append(self.margin(scores))
-
-#        # set up nodes
-#        o = torch.zeros([batch_size, 1, self.hidden_size], device=x.device)
-#        z_1Node = Node(z_1, name='z_1', structure=G_structure[0], left_child=Node(x, 'x'), right_child=Node(h_prev, 'h'))
-#        rNode = Node(r, name='r', structure=G_structure[1], left_child=Node(x, 'x'), right_child=Node(h_prev, 'h'))
-#        z_2Node = Node(z_2, name='z_2', structure=G_structure[2], left_child=Node(x, 'x'), right_child=Node(h_prev, 'h'))
-#        rhNode = Node(rh, name='r*h', structure=G_structure[3], left_child=Node(h_prev, 'h'), right_child=rNode)
-#        h_tildeNode = Node(h_tilde, name='h_tilde', structure=G_structure[4], left_child=Node(x, 'x'), right_child=rhNode)
-#        oneMinuszNode = Node(oneMinusZ1, name='1-z', structure=G_structure[5], left_child=Node(o, '0'), right_child=z_1Node)
-#        zh_tildeNode = Node(zh_tilde, name='(1-z)*h_tilde', structure=G_structure[6], left_child=h_tildeNode, right_child=oneMinuszNode)
-#        zhNode = Node(z2h, name='z*h', structure=G_structure[7], left_child=Node(h_prev, 'h'), right_child=z_2Node)
-#        h_nextNode = Node(h_next, name='h_next', structure=G_structure[8], left_child=zh_tildeNode, right_child=zhNode)
-
-#        G_node = [z_1Node, rNode, z_2Node, rhNode, h_tildeNode, oneMinuszNode, zh_tildeNode, zhNode, h_nextNode]
-#        for node in G_node:
-#            node.leftchild.parent = node
-#            node.rightchild.parent = node
-
-        G_node = torch.cat([z_1, r, z_2, rh, h_tilde, oneMinusZ1, zh_tilde, z2h, h_next], dim=2)
-        margins = torch.cat(margins, dim=1)
-        # shape: batch_size * 1 * (nodes_num*hidden_size)
-
-        return h_next, G_structure, G_node, margins
-
-
+        return v
+    
+    
 class RRNNforGRU(nn.Module):
     def __init__(self, hidden_size, vocab_size, batch_size, scoring_hsize=None):
         super(RRNNforGRU, self).__init__()
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
+        self.batch_size = batch_size
         self.cell = RRNNforGRUCell(hidden_size, scoring_hsize=scoring_hsize)
         self.output_layer = nn.Linear(hidden_size, vocab_size)
-        self.batch_size = batch_size
 
-    def init_hidden(self, n_batch, device=torch.device('cpu')):
-        return torch.zeros([n_batch, 1, self.hidden_size], requires_grad=True, device=device)
-
-    def forward(self, inputs):
-        time_steps = inputs.shape[1]
-        h_next = self.init_hidden(inputs.shape[0], device=inputs.device)
-        h_list = []
-        pred_tree_list = []
-        margins_list = []
-        structures_list = []
-        pred_chars_list = []
-
-        for t in range(time_steps):
-            x_t = inputs[:, t, :].reshape(-1, 1, self.hidden_size)
-            h_next, G_structure, G_node, margins = self.cell(x_t, h_next)
-
-            pred_chars_list.append(self.output_layer(h_next))
-            h_list.append(h_next)
-            structures_list.append(G_structure)
-            pred_tree_list.append(G_node)
-            margins_list.append(margins)
-
-        h_batch = torch.cat(h_list, dim=1)
-        pred_chars_batch = torch.cat(pred_chars_list, dim=1)
-        margins_batch = torch.stack(margins_list, dim=1)
-        return pred_chars_batch, h_batch, pred_tree_list, structures_list, margins_batch
+    def init_hidden(self, n_batch=None, device=torch.device('cpu')):
+        if n_batch is None:
+            return torch.zeros([self.batch_size, 1, self.hidden_size], requires_grad=True, device=device)
+        else:
+            return torch.zeros([n_batch, 1, self.hidden_size], requires_grad=True, device=device)
+        
+    def forward(self, inputs, G_structure=None):
+        batch_size, time_steps, _ = inputs.shape
+        h_next = self.init_hidden(batch_size, inputs.device)
+        
+        # stage 1
+        if G_structure is None:
+            h_list = []
+            pred_tree_list = []
+            margins_list = []
+            structures_list = []
+            pred_chars_list = []
+    
+            for t in range(time_steps):
+                x_t = inputs[:, t, :].reshape(-1, 1, self.hidden_size)
+                h_next, G_structure, G_node, G_margin = self.cell.forward_stage_search(x_t, h_next)
+                
+                pred_chars_list.append(self.output_layer(h_next))
+                h_list.append(h_next)
+                structures_list.append(G_structure)
+                pred_tree_list.append(G_node)
+                margins_list.append(G_margin)       
+            
+            print(len(margins_list), margins_list[0].shape)
+            
+            h_batch = torch.cat(h_list, dim=1)
+            pred_chars_batch = torch.cat(pred_chars_list, dim=1)
+            margins_batch = torch.stack(margins_list, dim=1)            
+            
+            return pred_chars_batch, h_batch, pred_tree_list, structures_list, margins_batch
+        # stage 2
+        else:
+            pred_chars_list = []
+            
+            for t in range(time_steps):
+                x_t = inputs[:, t, :].reshape(-1, 1, self.hidden_size)
+                h_next = self.cell.foward_stage_fixing(x_t, h_prev, G_structure)
+                pred_chars_list.append(self.output_layer(h_next))
+            
+            return pred_chars_list
 
 if __name__ == '__main__':
     timer = time.time()
     HIDDEN_SIZE = 100
+    batch_size = 16
     for i in range(1):
-        x = torch.randn(16, 1, HIDDEN_SIZE)
-        h_prev = torch.randn(16, 1, HIDDEN_SIZE)
+        x = torch.randn(batch_size, 1, HIDDEN_SIZE)
+        h_prev = torch.randn(batch_size, 1, HIDDEN_SIZE)
         cell = RRNNforGRUCell(HIDDEN_SIZE)
-        h_next, G_structure, G_node, margins = cell(x, h_prev)
+        h_next, G_structure, G_node, G_margin = cell.forward_stage_search(x, h_prev)
     print(time.time()-timer)
-
 
     # model
     timer = time.time()
@@ -317,10 +280,25 @@ if __name__ == '__main__':
         BATCH_SIZE = 16
         inputs = torch.randn(BATCH_SIZE, 20, HIDDEN_SIZE)
         model = RRNNforGRU(HIDDEN_SIZE, vocab_size=27, batch_size=BATCH_SIZE)
-        pred_chars_batch, h_batch, pred_tree_list, structures_list, margins_batch = model(inputs)
+        pred_chars_batch, h_batch, pred_tree_list, structures_list, margins_batch = model.forward(inputs)
     print(time.time()-timer)
 
+    GRU_structure = [['x', 'h_prev', 0, 'add', 'sigmoid'],
+                     ['x', 'h_prev', 1, 'add', 'sigmoid'],
+                     ['x', 'h_prev', 0, 'add', 'sigmoid'],
+                     ['h_prev', 'G[1]', 2, 'mul', 'identity'],
+                     ['x', 'G[3]', 2, 'add', 'tanh'],
+                     ['o', 'G[0]', 2, 'add', 'minus'],
+                     ['h_prev', 'G[2]', 3, 'mul', 'identity'],
+                     ['G[4]', 'G[5]', 3, 'mul', 'identity'],
+                     ['G[6]', 'G[7]', 3, 'add', 'identity']]
 
-
-
-
+    # model
+    timer = time.time()
+    for i in range(1):
+        BATCH_SIZE = 16
+        inputs = torch.randn(BATCH_SIZE, 20, HIDDEN_SIZE)
+        model = RRNNforGRU(HIDDEN_SIZE, vocab_size=27, batch_size=BATCH_SIZE)
+        pred_chars_list = model.forward(inputs, GRU_structure)
+    print(time.time()-timer)
+    
