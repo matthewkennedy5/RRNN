a16e603a6f7f9e4b9a082ca0be11ba28b6fef1a4
Xin Qian <44562654+Burururu@users.noreply.github.com>
2019-04-26 05:10:41 -0500

    load and save structures and losses

--- trainer_temp.py
+++ trainer_temp.py
@@ -25,7 +25,6 @@ import standard_data
 
 VOCAB_SIZE = 27
 HIDDEN_SIZE = 100
-n_epoch = 100
 time_steps = 20
 
 
@@ -60,22 +59,63 @@ params = {
             "device": "cpu"
         }
 
+GRU_structure = [['x', 'h_prev', 0, 'add', 'sigmoid'],
+                     ['x', 'h_prev', 1, 'add', 'sigmoid'],
+                     ['x', 'h_prev', 0, 'add', 'sigmoid'],
+                     ['h_prev', 'G[1]', 2, 'mul', 'identity'],
+                     ['x', 'G[3]', 2, 'add', 'tanh'],
+                     ['o', 'G[0]', 2, 'add', 'minus'],
+                     ['h_prev', 'G[2]', 3, 'mul', 'identity'],
+                     ['G[4]', 'G[5]', 3, 'mul', 'identity'],
+                     ['G[6]', 'G[7]', 3, 'add', 'identity']]
+
+def load_optimal_history(i_batch):
+    file_name = BATCH_HISTORY_DIR + '%d.txt'%i_batch
+    
+    if not os.path.isfile(file_name):
+        raise ValueError('No such batch file')
+    
+    loss_history = []
+    structure_history = []
+    
+    file = open(file_name, 'r')
+    for line in file.readlines():
+        i_epoch, i_batch, i_time_step, loss, structure = [eval(s) for s in line[:-1].split(';')]
+        if i_epoch+1 > len(loss_history):
+            loss_history.append([])
+            structure_history.append([])
+        loss_history[i_epoch].append(loss)
+        structure_history[i_epoch].append(structure)
+    file.close()
+    
+    loss_history = np.array(loss_history)
+    idx = np.argmin(loss_history, axis=0)
+    optimal_history = []
+    for i_time_step in range(time_steps):
+        optimal_history.append(structure_history[idx[i_time_step]][i_time_step])
+        
+    return optimal_history
+
+# hyper parameters
 batch_size = params['batch_size']
 device = params['device']
+train_stage = 'fixing'
+train_stage = 'searching' # another choice is 'fixing'
+lamb1, lamb2, lamb3, lamb4 = params['lambdas']
+loss = torch.nn.CrossEntropyLoss()
+
+# files
 dirname = 'test%s'%(time.asctime().replace(':', '_'))
 if not params['warm_start']:
     os.mkdir(dirname)
 os.chdir(dirname)
+BATCH_HISTORY_DIR = 'batch_history/'
+if not os.path.isdir(BATCH_HISTORY_DIR):
+    os.makedirs(BATCH_HISTORY_DIR)
 
+# models
 gru_model = torch.load('../gru_parameters.pkl')
 model = RRNNforGRU(HIDDEN_SIZE, VOCAB_SIZE, batch_size, params['scoring_hidden_size'])
-#filename = os.path.join('..', params['data_file'])
-
-train_set = standard_data.EnWik8Clean(subset='train', n_data=params['nb_train'], device=device)
-train_dataloader = DataLoader(train_set, batch_size=params['batch_size'], shuffle=True)
-lamb1, lamb2, lamb3, lamb4 = params['lambdas']
-loss = torch.nn.CrossEntropyLoss()
-
 if params['optimizer'] == 'adam':
     optimizer = torch.optim.Adam(model.parameters(), lr=params['learning_rate'])
 elif params['optimizer'] == 'sgd':
@@ -85,88 +125,109 @@ if params['device'] != 'cpu':
     gru_model = gru_model.cuda()
     model = model.cuda()
 
-model.train()
-gru_model.train()
+# load data
+train_set = standard_data.EnWik8Clean(subset='train', n_data=params['nb_train'], device=device)
+train_dataloader = DataLoader(train_set, batch_size=params['batch_size'], shuffle=False, drop_last=True)
+
+
+
 
 timer=time.time()
+model.train()
+gru_model.train()
+n_epoch = 20
 
 for i_epoch in range(n_epoch):
     i_batch = -1
     for X, y in train_dataloader:
         i_batch += 1
         optimizer.zero_grad()
-        
-        # forward pass
-        pred_chars_batch, h_batch, pred_tree_list, structures_list, margins_batch = model(X)
-        
-        # forward pass of traditional GRU
-        gru_h_list = gru_model(X)[0]
-        gru_h_list = torch.cat([torch.zeros([batch_size, 1, HIDDEN_SIZE], device=X.device), gru_h_list], dim=1)
-        target_tree_list = []
-        for t in range(X.shape[1]):
-            gru_x = X[:, t, :].reshape(batch_size, 1, HIDDEN_SIZE)
-            gru_h = gru_h_list[:, t, :].reshape(batch_size, 1, HIDDEN_SIZE)
-            target_tree = tree_methods.GRUtree_pytorch(gru_x, gru_h,
-                                                       gru_model.weight_ih_l0,
-                                                       gru_model.weight_hh_l0,
-                                                       gru_model.bias_ih_l0,
-                                                       gru_model.bias_hh_l0)[1]
-            target_tree_list.append(target_tree)
-        
-#        gru_h_list = gru_h_list[:, 1:, :]
-#        gru_loss1 = 0
-#        for i_time in range(y.shape[1]):
-#            gru_loss1 += loss(model.output_layer(gru_h_list[:, i_time, :]), torch.argmax(y[:, i_time, :], dim=1))
+        if train_stage == 'fixing':
+            optimal_structure = load_optimal_history(i_batch)
+            pred_chars_batch = model(X, optimal_structure)
             
-        # get weight for each loss terms
-        lamb1, lamb2, lamb3, lamb4 = params['lambdas']
-        
-        # calculate loss terms
-        loss1 = 0
-        if lamb1 != 0:
+            loss1 = 0
             for i_time in range(y.shape[1]):
-                loss1 += loss(pred_chars_batch[:, i_time, :], torch.argmax(y[:, i_time, :], dim=1))
-
-        loss2 = 0
-        if lamb2 != 0:
-            desired_margin = params['loss2_margin']
-            loss2 = (desired_margin - margins_batch.clamp(max=desired_margin)).sum()/desired_margin
+                    loss1 += loss(pred_chars_batch[:, i_time, :], torch.argmax(y[:, i_time, :], dim=1))
             
-        loss3 = 0
-        if lamb3 != 0:
+            loss3 = 0
             for param in model.parameters():
                 loss3 += param.norm()**2
-        
-        loss4 = 0
-        if lamb4 != 0:
-            for i_time_step in range(time_steps):
-                loss4 += tree_methods.tree_distance_metric_list(
-                                            pred_tree_list[i_time_step], 
-                                            target_tree_list[i_time_step])
 
+            losses = [lamb1*loss1, lamb3*loss3]
             
-#        losses = [lamb1*loss1]
-#        losses = [lamb1*loss1, lamb3*loss3]
-#        losses = [lamb1*loss1, lamb3*loss3, lamb4*loss4]
-        losses = [lamb1*loss1, lamb2*loss2, lamb3*loss3, lamb4*loss4]
-
-        accuracy = (pred_chars_batch.argmax(dim=2)==y.argmax(dim=2)).sum().item()/float(time_steps*batch_size)
-        
-        
+        if train_stage == 'searching':
+            # forward pass
+            pred_chars_batch, h_batch, pred_tree_list, structures_list, margins_batch = model(X)
+            
+            # forward pass of traditional GRU
+            gru_h_list = gru_model(X)[0]
+            gru_h_list = torch.cat([torch.zeros([batch_size, 1, HIDDEN_SIZE], device=X.device), gru_h_list], dim=1)
+            target_tree_list = []
+            for t in range(X.shape[1]):
+                gru_x = X[:, t, :].reshape(batch_size, 1, HIDDEN_SIZE)
+                gru_h = gru_h_list[:, t, :].reshape(batch_size, 1, HIDDEN_SIZE)
+                target_tree = tree_methods.GRUtree_pytorch(gru_x, gru_h,
+                                                           gru_model.weight_ih_l0,
+                                                           gru_model.weight_hh_l0,
+                                                           gru_model.bias_ih_l0,
+                                                           gru_model.bias_hh_l0)[1]
+                target_tree_list.append(target_tree)
+                
+            # get weight for each loss terms
+            lamb1, lamb2, lamb3, lamb4 = params['lambdas']
+            
+            # calculate loss terms
+            loss1_list = []
+            for i_time in range(y.shape[1]):
+                loss1_list.append(loss(pred_chars_batch[:, i_time, :], torch.argmax(y[:, i_time, :], dim=1)))
+            loss1 = sum(loss1_list)
+    
+            loss2 = 0
+            if lamb2 != 0:
+                desired_margin = params['loss2_margin']
+                loss2 = (desired_margin - margins_batch.clamp(max=desired_margin)).sum().div_(desired_margin)
+                
+            loss3 = 0
+            if lamb3 != 0:
+                for param in model.parameters():
+                    loss3 += param.norm()**2
+            
+            loss4 = 0
+            if lamb4 != 0:
+                loss4_list = []
+                for i_time_step in range(time_steps):
+                    loss4_list.append(tree_methods.tree_distance_metric_list(
+                                                pred_tree_list[i_time_step], 
+                                                target_tree_list[i_time_step]))
+                loss4 = sum(loss4_list)
+                    
+            losses = [lamb1*loss1, lamb2*loss2, lamb3*loss3, lamb4*loss4]
+            
+            # save batch history
+            file = open(BATCH_HISTORY_DIR+'%d.txt'%i_batch, 'a')
+            for i_time_step in range(time_steps):
+                lst = [i_epoch, i_batch, i_time_step, loss1_list[i_time_step].item(), structures_list[i_time_step]]
+                file.write(';'.join([str(s) for s in lst])+'\n')
+            file.close()
+                
+            
         # opt
         loss_fn = sum(losses)
         loss_fn.backward()
         optimizer.step()
+        
+        # print statistics        
+        accuracy = (pred_chars_batch.argmax(dim=2)==y.argmax(dim=2)).sum().item()/float(time_steps*y.shape[0])
         print('='*80)
         print(i_batch, time.time()-timer)
         print(i_epoch, i_batch, loss_fn.item(), time.time()-timer)
         print(losses)
-        print(accuracy, -np.log2(accuracy), loss1.item()/(20*np.log(2)))
+        bpc1 = -np.log2(accuracy)
+        bpc2 = loss1.item()/(20*np.log(2))
+        print(accuracy, bpc1, bpc2)
         # target bpc1: 1.17, bpc2: 2.73
     
-
-
-
 
 
 
