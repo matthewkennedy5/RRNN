332c1e23fcf39853c9bd940fa0574717d561dbfa
Xin Qian <44562654+Burururu@users.noreply.github.com>
2019-06-04 21:09:24 -0500

    Update standard_data.py

--- standard_data.py
+++ standard_data.py
@@ -5,17 +5,33 @@ import numpy as np
 import pdb
 from tqdm import tqdm
 from gensim.models import Word2Vec
+from torch.utils import data
+from torchtext import data, datasets
 from dataloader import element_dict
+from torch.utils.data import DataLoader
 
 dirname = os.path.dirname(os.path.realpath(__file__))
 CORPUS_FILENAME = os.path.join(dirname, 'enwik8_clean.txt')
-SAVE_FILE = os.path.join(dirname, '10k_data.pkl')
+SAVE_FILE = os.path.join(dirname, '10k_data_more.pkl')
 CHUNK_LENGTH = 20
 EMBEDDINGS = 'gensim'
-N_TRAIN = 5000
-N_VAL = 1000
-N_TEST = 2000
+N_TRAIN = 10000
+N_VAL = 10000
+N_TEST = 20000
 N_CHARS = len(element_dict)
+NORM_STATS_FILE = 'normalization.pkl'
+
+def load(file, dtype='float32'):
+    # file should contain a trailing newline
+    embeddings = {}
+    with open(file) as f:
+        for line in f:
+            char = line[0]
+            embeddings[char] = np.zeros(300)
+            for i, word in enumerate(line[1:].split()):
+                embeddings[char][i] = float(word)
+            embeddings[char] = embeddings[char].astype(dtype)
+    return embeddings
 
 # Load the embed dictionary that maps characters to vectors
 word2vec_model = Word2Vec.load('train20_embedding')
@@ -159,6 +175,9 @@ def load_standard_data():
     The point of this method is that if we're comparing RRNN to GRU, we want to
     use the same data. So we specifically store the data that we use so we can
     use it to train and evaluate both models instead of randomly selecting samples.
+
+    Returns:
+        data - Tuple of ((X_train, y_train), (X_val, y_val), (X_test, y_test)).
     """
     if not os.path.isfile(SAVE_FILE):
         print('[INFO] Partitioning fresh training, validation, and test data '
@@ -168,3 +187,152 @@ def load_standard_data():
     data = pickle.load(open(SAVE_FILE, 'rb'))
     return data
 
+
+def get_train_stats(dataset):
+    """Returns the mean and std of the train set as a 100-dimensional vector."""
+    if os.path.isfile(NORM_STATS_FILE):
+        stats = pickle.load(open(NORM_STATS_FILE, 'rb'))
+        return stats[dataset]
+
+    # Find train statistics from scratch.
+    print('[INFO] Calculating normalization statistics.')
+    ptb_train = PennTreebank('train', n_data=N_TRAIN, normalize=False)
+    sst_train = SST('train', n_data=N_TRAIN, normalize=False)
+    ptb_loader = DataLoader(ptb_train, batch_size=len(ptb_train))
+    sst_loader = DataLoader(sst_train, batch_size=len(sst_train))
+    ptb_X_train = next(iter(ptb_loader))[0]
+    sst_X_train = next(iter(sst_loader))[0]
+    ptb_X_train = torch.reshape(ptb_X_train, (-1, 100))
+    sst_X_train = torch.reshape(sst_X_train, (-1, 100))
+    ptb_mean = torch.mean(ptb_X_train, dim=0)
+    ptb_std = torch.std(ptb_X_train, dim=0)
+    sst_mean = torch.mean(sst_X_train, dim=0)
+    sst_std = torch.std(sst_X_train, dim=0)
+    stats = {
+        'ptb': (ptb_mean, ptb_std),
+        'sst': (sst_mean, sst_std)
+    }
+    pickle.dump(stats, open(NORM_STATS_FILE, 'wb'))
+    return get_train_stats(dataset)
+
+
+class EnWik8Clean(data.Dataset):
+    """Dataset containing the enwik8_clean.txt 27-character Wikipedia data.
+
+    Inputs:
+        subset - either 'train', 'val', or 'test'.
+        n_data - How many data points to sample (with a maximum determined by
+            the size of the data in the pickle file.)
+        device
+    """
+
+    def __init__(self, subset, n_data, device=torch.device('cpu')):
+        train, val, test = load_standard_data()
+        if subset == 'train':
+            self.X, self.y = train
+        elif subset == 'val':
+            self.X, self.y = val
+        elif subset == 'test':
+            self.X, self.y = test
+        else:
+            raise ValueError('Subset input must be "train", "val", or "test"')
+
+        self.X = self.X[:n_data].to(device)
+        self.y = self.y[:n_data].to(device)
+        self.y = torch.argmax(self.y, dim=2)  # Convert to index labels
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, index):
+        return (self.X[index], self.y[index])
+
+
+class PennTreebank(data.Dataset):
+    """Dataset containing the Penn Treebank word-level language modelling data.
+
+    Inputs:
+        subset - either 'train', 'val', or 'test'.
+        n_data - how many data points to sample.
+        device - torch.device on which to store the data
+        normalize - Whether to normalize the data based on the training statistics.
+    """
+    def __init__(self, subset, n_data, device=torch.device('cpu'), normalize=True):
+        train, valid, test = datasets.PennTreebank.iters(batch_size=1,
+                                                         bptt_len=CHUNK_LENGTH,
+                                                         device=device,
+                                                         vectors='glove.6B.100d')
+        if subset == 'train':
+            self.data = train
+        elif subset == 'val':
+            self.data = valid
+        elif subset == 'test':
+            self.data = test
+        self.embeddings = self.data.dataset.fields['text'].vocab.vectors
+
+        self.X = []
+        self.y = []
+        for sample in self.data:
+            # sample.text contains the index of the embedded vector. We store the
+            # vectors themselves in self.X, and word indices in self.y.
+            if len(sample.text) == CHUNK_LENGTH:    # To avoid the leftover chunk of 8 at the end
+                self.X.append(self.embeddings[sample.text])
+                self.y.append(sample.target)
+        self.X = torch.stack(self.X[:n_data]).squeeze().to(device)
+        self.y = torch.stack(self.y[:n_data]).squeeze().to(device)
+
+        if normalize:
+            mean, std = get_train_stats('ptb')
+            self.X = (self.X - mean.to(device)) / std.to(device)
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, index):
+        return (self.X[index], self.y[index])
+
+class SST(data.Dataset):
+    """Dataset containing the Stanford Sentiment Treebank (SST) dataset.
+
+    Inputs:
+        subset - either 'train', 'val', or 'test'.
+        n_data - how many data points to sample.
+        device - torch.device on which to store the data
+    """
+    def __init__(self, subset, n_data, device='cpu', normalize=True):
+        train, valid, test = datasets.SST.iters(batch_size=1, device=device,
+                                                vectors='glove.6B.100d')
+
+        if subset == 'train':
+            self.data = train
+        elif subset == 'val':
+            self.data = valid
+        elif subset == 'test':
+            self.data = test
+        self.embeddings = self.data.dataset.fields['text'].vocab.vectors
+
+        self.X = []
+        self.y = []
+        MAX_LENGTH = 52     # Longest entry in the SST dataset
+        embed_size = self.embeddings.shape[1]
+        for sample in self.data:
+            # Zero pad the sample since they have different lengths. I'm zero
+            # padding the front of each sequence.
+            embedded_sample = self.embeddings[sample.text]
+            newX = torch.zeros(MAX_LENGTH, 1, embed_size)
+            n_pad = MAX_LENGTH - embedded_sample.shape[0]
+            newX[n_pad:, :, :] = embedded_sample
+            self.X.append(newX)
+            self.y.append(sample.label - 1)     # Convert from 1, 2, 3 to 0, 1, 2
+
+        self.X = torch.stack(self.X[:n_data]).squeeze().to(device)
+        self.y = torch.stack(self.y[:n_data]).squeeze().to(device)
+        if normalize:
+            mean, std = get_train_stats('sst')
+            self.X = (self.X - mean) / std
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
